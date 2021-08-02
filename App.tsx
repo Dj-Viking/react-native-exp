6ae3df128363332e28939afbbdbf400e78d77a3e
dj-viking <anders.swedishviking@gmail.com>
2021-08-01 23:05:57 -0400

    experimenting

--- App.tsx
+++ App.tsx
@@ -1,21 +1,47 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, TextInput, Button, ButtonProps, NativeSyntheticEvent, NativeTouchEvent } from 'react-native';
+import Input from "./components/Input";
+
+
 
 export default function App() {
+
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: 'grey',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+  });
+
+  const todos = [
+    {
+      text: "heres a todo",
+      color: "white"
+    },
+    {
+      text: "todoo 2",
+      color: "white"
+    }
+  ];
+  
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      
+      {
+        todos.map(todo => (
+          <Text style={{ color: todo.color }}> {todo.text}</Text>
+        ))
+      }
+
+      <Input/>
+
       <StatusBar style="auto" />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
